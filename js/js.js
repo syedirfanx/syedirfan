c466dde7ff5851890002cf8b0653170f8f8eee93
Erfanx <irfansyed479@gmail.com>
2020-06-21 01:23:29 +0600

    Update cover

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText([ 'This is Syed Irfan.', 'I do analyze information.'], 'text',['#b8c1de']);
+ consoleText([ 'This is Syed Irfan.', 'I do analyze information.', 'The big data knows everything.', 'For the betterment of humanity.','Where every search has a value.','Everything is fair in acquiring information.'], 'text',['#b8c1de']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
@@ -24,19 +24,19 @@ function consoleText(words, id, colors) {
         target.setAttribute('style', 'color:' + colors[0])
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 10)
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 2500)
     } else if (waiting === false) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
-  }, 120)
+  }, 50)
   window.setInterval(function() {
     if (visible === true) {
       con.className = 'console-underscore hidden'
