04823c5c1be2ee119dd28ebd778a583ce417154d
Erfanx <irfansyed479@gmail.com>
2020-09-21 02:22:00 +0600

    Update site

--- js/js.js
+++ js/js.js
@@ -1,51 +1,58 @@
-// function([string1, string2],target id,[color1,color2])    
- consoleText([ 'This is Syed Irfan.', 'I do analyze information.', 'The big data knows everything.', 'For the betterment of humanity.','Where every search has a value.','Everything is fair in acquiring information.'], 'text',['#b8c1de']);
+// function([string1, string2],target id,[color1,color2])
+consoleText(
+  [
+    "This is Syed Irfan.",
+    "Welcome to my personal website.",
+    "I compose spells for computers.",
+    "I love watching movies.",
+  ],
+  "text",
+  ["#b8c1de"]
+);
 
 function consoleText(words, id, colors) {
-  if (colors === undefined) colors = ['#fff'];
+  if (colors === undefined) colors = ["#fff"];
   var visible = true;
-  var con = document.getElementById('console');
+  var con = document.getElementById("console");
   var letterCount = 1;
   var x = 1;
   var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
-
+  var target = document.getElementById(id);
+  target.setAttribute("style", "color:" + colors[0]);
+  window.setInterval(function () {
     if (letterCount === 0 && waiting === false) {
       waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
         var usedColor = colors.shift();
         colors.push(usedColor);
         var usedWord = words.shift();
         words.push(usedWord);
         x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
+        target.setAttribute("style", "color:" + colors[0]);
         letterCount += x;
         waiting = false;
-      }, 10)
+      }, 10);
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 2500)
+      }, 2500);
     } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       letterCount += x;
     }
-  }, 50)
-  window.setInterval(function() {
+  }, 50);
+  window.setInterval(function () {
     if (visible === true) {
-      con.className = 'console-underscore hidden'
+      con.className = "console-underscore hidden";
       visible = false;
-
     } else {
-      con.className = 'console-underscore'
+      con.className = "console-underscore";
 
       visible = true;
     }
-  }, 400)
+  }, 400);
 }
