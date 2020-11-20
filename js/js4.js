8ce7eace698739a40456b21982f4e6eaa4915a5c
Erfanx <irfansyed479@gmail.com>
2020-11-20 22:26:40 +0600

    Update site

--- js/js4.js
+++ js/js4.js
@@ -1,54 +1,17 @@
-// NOTE:
-// Run to install required library
-// $ npm i --save bigpicture
-
-/** FRONTEND SCRIPT **/
-
-// Uncomment when adding into the project.
-// import domReady from '@wordpress/dom-ready'
-// import BigPicture from 'bigpicture'
-
-domReady(() => {
-	const elems = document.querySelectorAll(".ugb-video-popup");
-	const openVideo = (el) => {
-		if (BigPicture) {
-			const videoID = el.getAttribute("data-video");
-			const args = {
-				el,
-				noLoader: true
-			};
-			if (videoID.match(/^\d+$/g)) {
-				args["vimeoSrc"] = videoID;
-			} else if (videoID.match(/^https?:\/\//g)) {
-				args["vidSrc"] = videoID;
-			} else {
-				args["ytSrc"] = videoID;
-			}
-			BigPicture(args);
-		}
-	};
-	elems.forEach((el) => {
-		const a = el.querySelector("a");
-		a.addEventListener("click", (ev) => {
-			ev.preventDefault();
-			openVideo(el);
-		});
-		a.addEventListener("touchend", (ev) => {
-			ev.preventDefault();
-			openVideo(el);
-		});
-	});
+$(document).ready(function() {
+  autoPlayYouTubeModal();
 });
 
-/** DO NOT COPY **/
-function domReady(fn) {
-	if (
-		document.attachEvent
-			? document.readyState === "complete"
-			: document.readyState !== "loading"
-	) {
-		fn();
-	} else {
-		document.addEventListener("DOMContentLoaded", fn);
-	}
-}
+function autoPlayYouTubeModal() {
+  var trigger = $('.trigger');
+  trigger.click(function(e) {
+    e.preventDefault();
+    var theModal = $(this).data("target");
+    var videoSRC = $(this).attr("src");
+    var videoSRCauto = videoSRC + "?autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal).on('hidden.bs.modal', function(e) {
+      $(theModal + ' iframe').attr('src', '');
+    });
+  });
+};
